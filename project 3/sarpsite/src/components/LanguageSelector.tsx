0757e6299158b3e4044c2933a1f376f11513ecf8
sarpkarakaya1 <sarpkarakaya@aol.com>
2025-07-11 12:21:41 +0200

    Fix directory navigation for npm commands

--- project 3/sarpsite/src/components/LanguageSelector.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const LanguageSelector = () => {
-  // Empty component - no language selector needed for English-only
-  return null;
-};
-
-export default LanguageSelector;
